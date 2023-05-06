abef07b465fa1f219e2aafa49ad0cedecce27cdf
清汤白面 <lizhao@eelantech.com>
2023-05-06 09:41:58 +0800

    [Feature] 20230505
    [Reason 修改原因]:
    [Solution 解决方案/修改思路]：
    [Effect 修改影响说明]:

---
+++ src/main/java/cn/qingtangbaimian/algorthm/string/RemoveDuplicate.java
@@ -0,0 +1,33 @@
+package cn.qingtangbaimian.algorthm.string;
+
+/**
+ * @author <a href="mailto:is_fire_subscribe@hotmail.com">李昭</a>
+ * @description
+ * @date 2023-05-05 10:47:07
+ */
+public class RemoveDuplicate {
+
+    public static void main(String[] args) {
+        String s = "abbaca";
+        System.out.println(removeDuplicates(s));
+    }
+
+    public static String removeDuplicates(String s) {
+        StringBuilder builder = new StringBuilder();
+        char[] chars = s.toCharArray();
+        char lastChar = 0;
+        for (char aChar : chars) {
+            if (builder.length() == 0 || lastChar == 0) {
+                lastChar = aChar;
+                builder.append(lastChar);
+                continue;
+            }
+            builder = lastChar == aChar ? builder.deleteCharAt(builder.length() - 1)
+                    : builder.append(aChar);
+            lastChar = lastChar == aChar ?
+                    builder.length() > 0 ? builder.substring(builder.length() - 1).toCharArray()[0] : 0 :
+                    aChar;
+        }
+        return builder.toString();
+    }
+}
