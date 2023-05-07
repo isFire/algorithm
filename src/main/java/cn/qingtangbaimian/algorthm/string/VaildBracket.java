a3ef1f5f3c58a613763a81e6e3e10a5c3a5c7700
清汤白面 <is_fire_subscribe@hotmail.com>
2023-05-07 23:41:32 +0800

    [Feature] 20230507

---
+++ src/main/java/cn/qingtangbaimian/algorthm/string/VaildBracket.java
@@ -0,0 +1,48 @@
+package cn.qingtangbaimian.algorthm.string;
+
+import java.util.Stack;
+
+/**
+ * @author <a href="mailto:is_fire_subscribe@hotmail.com">清汤白面</a>
+ * @description
+ * @date 2023-05-07 23:21:55
+ */
+public class VaildBracket {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Byte> stack = new Stack<>();
+        char[] brackets = s.toCharArray();
+        for (char bracket : brackets) {
+            byte index = getIndex(bracket);
+            if (index <= 0) {
+                return false;
+            }
+            if (index <= 3) {
+                stack.push(index);
+                continue;
+            }
+            if (stack.isEmpty() || stack.pop() != index - 3) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public static byte getIndex(char bracket) {
+        switch (bracket){
+            case '{': return 1;
+            case '[': return 2;
+            case '(': return 3;
+            case '}': return 4;
+            case ']': return 5;
+            case ')': return 6;
+            default: return 0;
+        }
+    }
+
+}
