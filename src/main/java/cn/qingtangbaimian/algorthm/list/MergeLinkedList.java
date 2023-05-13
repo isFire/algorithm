b0d61a17fb7d58f8fb76995766c4c3335cfd9846
清汤白面 <is_fire_subscribe@hotmail.com>
2023-05-13 23:00:25 +0800

    [Feature] 20230513

--- src/main/java/cn/qingtangbaimian/algorthm/list/MergeLinkedList.java
+++ src/main/java/cn/qingtangbaimian/algorthm/list/MergeLinkedList.java
@@ -35,11 +35,5 @@ public class MergeLinkedList {
     }
 
 
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
+
 }
