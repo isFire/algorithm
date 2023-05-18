9f474c0efdf79c4fba5a2389e2b6db41ffee862b
清汤白面 <lizhao@eelantech.com>
2023-05-18 11:11:30 +0800

    [Feature] 20230518
    [Reason 修改原因]:
    [Solution 解决方案/修改思路]：
    [Effect 修改影响说明]: 说明修改的影响范围，回归测试需要注意的事项

--- src/main/java/cn/qingtangbaimian/algorthm/string/BinarySum.java
+++ src/main/java/cn/qingtangbaimian/algorthm/string/BinarySum.java
@@ -8,7 +8,7 @@ package cn.qingtangbaimian.algorthm.string;
 public class BinarySum {
 
     public static void main(String[] args) {
-        System.out.println(addBinary1("1010", "1011"));
+        System.out.println(addBinary2("11", "1"));
     }
 
     public static String addBinary2(String a, String b) {
@@ -18,15 +18,8 @@ public class BinarySum {
         StringBuilder builder = new StringBuilder();
         while (aIndex >= 0 || bIndex >= 0) {
             int num = 0;
-            char aChar;
-            if (aIndex >= 0) {
-                aChar = a.charAt(aIndex--);
-                num += aChar - '0';
-            }
-            if (bIndex >= 0) {
-                aChar = b.charAt(bIndex--);
-                num += aChar - '0';
-            }
+            num += aIndex >= 0 ? a.charAt(aIndex--) - '0' : 0;
+            num += bIndex >= 0 ? b.charAt(bIndex--) - '0' : 0;
             num += mark;
             mark = 0;
             if (num >= 2) {
